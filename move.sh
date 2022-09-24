f49499c42ad909dfe29dc362a662cbb29391e8ac
steven <steven220705@gmail.com>
2022-09-24 16:50:20 +0700

    change move

--- move.sh
+++ move.sh
@@ -1,9 +1,15 @@
 echo "###delete files###"
 cd ../xinqiu-h5 
+git checkout dev
 rm -rf *
 echo "###copy files###"
 cp -rf ../wanqiu-v3/* ../xinqiu-h5
 cp ./.env.development ../xinqiu-h5/.env.development
 cp ./.env.production ../xinqiu-h5/.env.production
+cp ./.env.production.prod ../xinqiu-h5/.env.production.prod
 echo "### done###"
+
+git add .
+git commit -m"bash update"
+git push origin dev
 
