def229192e105d77f0e0bc0f9eb0bfabbaeca87a
steven <steven220705@gmail.com>
2022-10-12 14:39:08 +0700

    auto build 20221012-143908

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-rm -f jqiu-dist.zip
-rm -f jincheng-dist.zip
-rm -f halan-dist.zip
+# 删除目录
+rm -rf ./build/
+
+# 重新创建目录
+mkdir build
 
 # 编译金球
 npm run build # 编译测试环境
@@ -8,17 +10,15 @@ zip -q -r -m ./build/jqiu-dist.zip ./dist
 echo "金球打包完成"
 
 # 编译金橙
-## 改index
-mv ./index.html ./index.html.jq
-mv ./index.html.jc ./index.html
+## 移动金球的东西
+mv ./.env.production ./platform/jqiu/
+mv ./index.html ./platform/jqiu/
+mv ./src/assets/logo@2x.png ./platform/jqiu/
 
-## 改环境变量
-mv ./.env.production ./.env.production.jq
-mv ./.env.production.jc ./.env.production
-
-## 改logo
-mv ./src/assets/logo@2x.png ./src/assets/logo@2x.jq.png
-mv ./src/assets/logo@2x.jc.png ./src/assets/logo@2x.png
+## 添加金橙的东西
+mv ./platform/jcheng/index.html ./
+mv ./platform/jcheng/.env.production ./
+mv ./platform/jcheng/logo@2x.png ./src/assets/
 
 ## 编译打包
 npm run build
@@ -27,34 +27,38 @@ zip -q -r -m ./build/jincheng-dist.zip ./dist
 echo "金橙打包完成"
 
 # 编译哈兰
-mv ./index.html ./index.html.jc
-mv ./index.html.hl ./index.html
+## 移动金橙的东西
+mv ./.env.production ./platform/jcheng/
+mv ./index.html ./platform/jcheng/
+mv ./src/assets/logo@2x.png ./platform/jcheng/
 
-mv ./.env.production ./.env.production.jc
-mv ./.env.production.hl ./.env.production
-
-mv ./src/assets/logo@2x.png ./src/assets/logo@2x.jc.png
-mv ./src/assets/logo@2x.hl.png ./src/assets/logo@2x.png
+## 添加哈兰的东西
+mv ./platform/halan/index.html ./
+mv ./platform/halan/.env.production ./
+mv ./platform/halan/logo@2x.png ./src/assets/
 
 npm run build
 zip -q -r -m ./build/halan-dist.zip ./dist
 echo "哈兰打包完成"
 
 # 还原文件
-mv ./index.html ./index.html.hl
-mv ./index.html.jq ./index.html
 
-mv ./.env.production ./.env.production.hl
-mv ./.env.production.jq ./.env.production
+## 移动哈兰的东西
+mv ./.env.production ./platform/halan/
+mv ./index.html ./platform/halan/
+mv ./src/assets/logo@2x.png ./platform/halan/
 
-mv ./src/assets/logo@2x.png ./src/assets/logo@2x.hl.png
-mv ./src/assets/logo@2x.jq.png ./src/assets/logo@2x.png
+## 添加金球的东西
+mv ./platform/jqiu/index.html ./
+mv ./platform/jqiu/.env.production ./
+mv ./platform/jqiu/logo@2x.png ./src/assets/
+
 echo "文件还原完成"
 
 # 推送到git
 time=$(date "+%Y%m%d-%H%M%S")
 git add .
 git commit -m"auto build ${time}"
-git push origin simple-h5
+git push origin full-h5
 
 
