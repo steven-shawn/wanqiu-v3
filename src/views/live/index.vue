f7278b4c9dbd967198b010e1e372e21a8ccf5dec
steven <steven220705@gmail.com>
2022-10-20 16:54:09 +0700

    auto build 0.0.8

--- src/views/live/index.vue
+++ src/views/live/index.vue
@@ -8,7 +8,7 @@ div.pb-16
     div.flex.flex-wrap.justify-between
       video-list-item(v-for="item in state.hotList" :key="item.matchId" :item="item")
     van-loading.text-center.pt-12(type="spinner" v-if="state.hotLoading")
-    van-empty(description="暂无热门直播" image-size="0" v-if="!state.hotLoading && !state.hotList?.length")
+    van-empty(description="暂无热门直播" image-size="0" v-if="!state.hotLoading && !state.hotList?.length" style="padding-top: 10px;")
   div.now.px-2.mt-2(style="min-height: 192px;")
     video-list-title(type="now")
     div.flex.flex-wrap.justify-between
